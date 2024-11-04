bff0a872f4e6c9837c90b53020fa49ac59ea6c68
matas234 <160629386+matas234@users.noreply.github.com>
2024-11-04 13:19:47 +0000

    final

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from shanten_calculator import calculateShanten
 
 def webToArray(hand_string):
     cur_arr = []
-    out_arr = [[0]*9 for _ in range(3)] + [[0]*7]
+    out_arr = [0]*34
 
     suit_to_idx = {'m': 0, 'p': 1, 's': 2, 'z': 3}
 
@@ -11,7 +11,8 @@ def webToArray(hand_string):
         if chr in ['s', 'm', 'p', 'z']:
             suit_idx = suit_to_idx[chr]
             for tile_idx in cur_arr:
-                out_arr[suit_idx][tile_idx - 1] += 1
+                out_arr[suit_idx * 9 + tile_idx - 1] += 1
+
             cur_arr = []
 
         else:
@@ -30,11 +31,6 @@ with open('shanten_tests.txt', 'r') as f:
         hand_array = webToArray(hand_string)
         arrays.append(hand_array)
         calculated_shanten = calculateShanten(hand_array)
-
-        # print(f"hand: {hand_string}")
-        # print(f"true shanten: {true_shanten}")
-        # print(f"calculated shanten: {calculated_shanten}")
-        # print("")
 
         if calculated_shanten == int(true_shanten):
             passed += 1
